ae7d5b78c37ea0fa5c6b83f1122dd98077661c5c
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-11-22 19:09:16 +0800

    Update image sources and remove sections in index page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <div className="flex items-stretch justify-between gap-5 max-md:flex-wrap px-14 pb-4 pt-10">
       <img
         loading="lazy"
-        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/f1ac124f-e02f-40f0-91ad-70b573b1c50a?apiKey=a9c78a43f27145ccb5f12b07f1adc09a&" className="aspect-[2.37] object-contain object-center w-[97px] overflow-hidden shrink-0 max-w-full"
+        src="/newheroimgs/Creative Legazpi_LogoOutline.svg" className="aspect-[2.37] object-contain object-center w-[97px] overflow-hidden shrink-0 max-w-full"
         alt="Image description"
       />
       <div className="flex items-start justify-between gap-14 px-5 max-md:max-w-full max-md:flex-wrap max-md:justify-center">
