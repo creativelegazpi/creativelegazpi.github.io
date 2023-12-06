72d36fab2029a6d7ea46f02498002f998deee2ef
Bernard Historillo <bernardhistorillo1@gmail.com>
2023-12-06 22:24:52 +0800

    Add teaser video

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function TeaserSection() {
     return (
-        <div id='teaser' className='w-full lg:h-screen md:h-[25vh] flex justify-center items-center relative'>
-            <h1 className='absolute text-white bottom-10 left-10 text-whites z-10 font-bold text-2xl'>Mukna 2023 | 8 - 10 Dec 2023</h1>
-            <img
-                src='/newheroimgs/secondsection.png'
-                alt='video'
-                className='w-full h-full absolute lg:object-cover md:object-contain'
-            />
+        <div id='teaser'>
+            <div className="relative w-full overflow-hidden" style={{paddingTop:'56.25%'}}>
+                <iframe className="absolute top-0 left-0 w-full h-full"
+                    src="https://www.youtube.com/embed/tusnA3Z_Eis?autoplay=1&mute=1"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen></iframe>
+            </div>
         </div>
     )
 }
