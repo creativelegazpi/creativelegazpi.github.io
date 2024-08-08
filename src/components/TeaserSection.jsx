80b4a6d6d1e9eb19a97bf6dddf5452209843b7e8
iamveronica <hello@creativelegazpi.ph>
2024-08-08 16:44:44 +0800

    Update TeaserSection.jsx

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function TeaserSection() {
     return (
         <div id='teaser'>
-            <div className="relative w-full overflow-hidden" style={{paddingTop:'56.25%'}}>
-            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flegazpicitytourismoffice%2Fposts%2Fpfbid026jQLHzVBLGroSrh45DWHj8ZpowtCzasYuajCyxpErUnj5tt8YGAwdQRjZBpUsLvjl&show_text=true&width=500" width="500" height="392" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flegazpicitytourismoffice%2Fposts%2Fpfbid026jQLHzVBLGroSrh45DWHj8ZpowtCzasYuajCyxpErUnj5tt8YGAwdQRjZBpUsLvjl&show_text=true&width=500" 
+            width="500" height="392" style="border:none;overflow:hidden"
+             scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
             </div>
-        </div>
+  
     )
 }
 
