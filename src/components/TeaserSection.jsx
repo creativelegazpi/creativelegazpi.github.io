6953cd815de12169b8592c078c371b1fef5d4f55
iamveronica <hello@creativelegazpi.ph>
2024-08-08 16:55:34 +0800

    Update TeaserSection.jsx

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -5,7 +5,7 @@ function TeaserSection() {
         <div id='teaser'>
             <div className="relative w-full overflow-hidden" style={{paddingTop:'56.25%'}}>
                 <iframe className="absolute top-0 left-0 w-full h-full"
-                    src="https://fb.watch/tQfub8QlaK/"
+                    src="https://www.facebook.com/MutyangIbalongOfficialPage/videos/8501705989856741/"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen></iframe>
