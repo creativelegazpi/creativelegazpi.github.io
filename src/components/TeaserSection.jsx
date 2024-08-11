6a3e66f4c6073e110d94349b3a547301c7160565
iamveronica <hello@creativelegazpi.ph>
2024-08-12 07:41:41 +0800

    Update TeaserSection.jsx

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 function TeaserSection() {
     return (
         <div id='teaser'>
-            <div className="relative w-full overflow-hidden" style={{paddingTop:'56.25%'}}>
+            <div className="relative w-full overflow-hidden" >
                 <iframe className="absolute top-0 left-0 w-full h-full"
                     src="https://www.facebook.com/MutyangIbalongOfficialPage/videos/8501705989856741/"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen></iframe>
             </div>
         </div>
