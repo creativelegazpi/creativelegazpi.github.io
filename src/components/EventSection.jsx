423dd45682ba879d32a4bd5302e72f7af454ec8b
iamveronica <hello@creativelegazpi.ph>
2024-08-12 13:47:44 +0800

    Update EventSection.jsx

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -35,7 +35,7 @@ function EventSection() {
                         <div className="text-black text-center lg:text-right text-[1.6em] lg:text-[1.3em] xl:text-[1.4em] font-extrabold leading-6 mt-10 mb-[60px] leading-[1.3em]">
                          Legazpi City
                             <br />
-                                August 8-31,
+                                August 8-31, 2024
                         </div>
 
                         <div className="flex justify-center lg:justify-end">
