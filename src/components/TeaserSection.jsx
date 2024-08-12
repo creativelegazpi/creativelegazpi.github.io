c28e18045a23f23b52af59778495226ddd5002ac
iamveronica <hello@creativelegazpi.ph>
2024-08-12 08:28:10 +0800

    Update TeaserSection.jsx

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -5,9 +5,9 @@ function TeaserSection() {
         <div id='teaser'>
             <div className="relative w-full overflow-hidden" style={{paddingTop:'56.25%'}}>
                 <iframe className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/watch?v=qTpyb8lsXf4"
+                    src="https://www.youtube.com/embed/tusnA3Z_Eis?autoplay=1&mute=1"
                     title="YouTube video player" frameBorder="0"
-        
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen></iframe>
             </div>
         </div>
