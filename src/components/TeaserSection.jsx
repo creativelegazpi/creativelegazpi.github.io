f57be6eadb8bda140618911f21d9b22bf5876234
iamveronica <hello@creativelegazpi.ph>
2024-08-12 08:26:50 +0800

    Update TeaserSection.jsx

--- src/components/TeaserSection.jsx
+++ src/components/TeaserSection.jsx
@@ -7,7 +7,7 @@ function TeaserSection() {
                 <iframe className="absolute top-0 left-0 w-full h-full"
                     src="https://www.youtube.com/watch?v=qTpyb8lsXf4"
                     title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        
                     allowFullScreen></iframe>
             </div>
         </div>
